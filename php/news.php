1c60c7a17260f09fecb905b423dfc069120ca4c4
Савушкин Андрей Александрович <u0415418@TAOM.LAN>
2016-05-25 14:59:26 +0400

    news load image

--- php/news.php
+++ php/news.php
@@ -3,7 +3,7 @@
 	include "func.inc";
 	conect_DB();
 	
-	$sql=mysql_query("SELECT id_teacher,title,context FROM news");
+	$sql=mysql_query("SELECT * FROM news");
 	
 	$err = '';
 	
@@ -19,19 +19,45 @@
 		$sql_user=mysql_query("SELECT name,lastname FROM users WHERE id_user = '$id_user' ");
 		$row_user = mysql_fetch_array($sql_user);
 			
-		echo $row_user['name'];
-		print("   ");
-		echo $row_user['lastname'];
-		print("   ");
-		echo $row['title'];
-		print("   ");
-		echo $row['context'];
-		print("   ");
-		echo $row['id_teacher'];
-		print("<br>");
+		$title = $row['title'];
+		$context = $row['context'];
+		$time = $row['current_time'];
+		$image = $row['image'];
+		$name = $row_user['name'];
+		$lastname = $row_user['lastname'];
 			
+		print("<div class=\"center_content\">"); 
+
+		print("<div class=\"leftbox\">"); 
+
+		print("<h2>$title</h2>");
+				
+
+		print("<img src=\"http://cs543104.vk.me/v543104766/ef18/UAfy-frPW24.jpg\" />"); 
+
+		print("<p>"); 
+		print("$context <br>");
+		print("<a href=\"#\" class=\"more\">читать полностью</a>");
+		print("</p>"); 
+		
+		print("$name ");
+		print("$lastname ");
+		print("$time <br>");
+
+		 
+
+		print("</div>"); 
 			
-			
+		// echo $row_user['name'];
+		// print("   ");
+		// echo $row_user['lastname'];
+		// print("   ");
+		// echo $row['title'];
+		// print("   ");
+		// echo $row['context'];
+		// print("   ");
+		// echo $row['id_teacher'];
+		// print("<br>");
 			
 	}
 ?>
