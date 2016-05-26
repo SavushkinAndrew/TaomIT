ef488ec4c42760200c2725b622217b965039da6a
Савушкин Андрей Александрович <u0415418@TAOM.LAN>
2016-05-26 09:29:14 +0400

    sql join news

--- php/news.php
+++ php/news.php
@@ -3,28 +3,18 @@
 	include "func.inc";
 	conect_DB();
 	
-	$sql=mysql_query("SELECT * FROM news");
+	$sql=mysql_query("SELECT * FROM `teacher` INNER JOIN `news` ON `teacher`.`id_teacher` = `news`.`id_teacher`");
 	
 	$err = '';
 	
 	while ($row = mysql_fetch_array($sql)) {
 		
-		$id_teacher = $row['id_teacher'];
-						
-		$sql_teacher=mysql_query("SELECT user FROM teacher WHERE id_teacher = $id_teacher");
-		$row_teacher = mysql_fetch_array($sql_teacher);
-
-		$id_user = $row_teacher['user'];
-			
-		$sql_user=mysql_query("SELECT name,lastname FROM users WHERE id_user = '$id_user' ");
-		$row_user = mysql_fetch_array($sql_user);
-			
 		$title = $row['title'];
 		$context = $row['context'];
 		$time = $row['current_time'];
 		$image = $row['image'];
-		$name = $row_user['name'];
-		$lastname = $row_user['lastname'];
+		$name = $row['name'];
+		$lastname = $row['lastname'];
 			
 		print("<div class=\"center_content\">"); 
 
@@ -47,17 +37,5 @@
 		 
 
 		print("</div>"); 
-			
-		// echo $row_user['name'];
-		// print("   ");
-		// echo $row_user['lastname'];
-		// print("   ");
-		// echo $row['title'];
-		// print("   ");
-		// echo $row['context'];
-		// print("   ");
-		// echo $row['id_teacher'];
-		// print("<br>");
-			
 	}
 ?>
